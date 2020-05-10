e8ccb0e3ad6b0b55a7508926ed91e0a5312086e0
caixinquan <caixinquan@dfl.com.cn>
2020-05-10 15:59:50 +0800

    feat(app): 完成通用全局配置

--- src/app.js
+++ src/app.js
@@ -3,4 +3,13 @@ App({
     // 第一次打开
     console.info('App onLaunch');
   },
+  onShow() {
+    // 切入前台
+    console.log('App onShow');
+  },
+  onHide() {
+    // 切出至后台
+    console.log('App onHide');
+  },
+  globalData: {},
 });
