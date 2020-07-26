9156c9234502e49814f2217cf2eeb3df09e77b01
Oc-master <997787659@qq.com>
2020-07-26 20:17:13 +0800

    feat(pages/index): 完成域名、路由映射提示按钮布局

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,3 +1,27 @@
+import './index.less';
+
 Page({
-  data: {},
+  data: {
+    isShowDialog: false,
+    dialog: {
+      title: '',
+      content: '',
+    },
+  },
+  onClick(e) {
+    const dicts = {
+      host: {
+        title: '域名 hosts',
+      },
+      route: {
+        title: '路由 routes',
+      },
+    };
+    const { type } = e.target.dataset;
+    const { title } = dicts[type];
+    this.setData({
+      isShowDialog: true,
+      dialog: { title },
+    });
+  },
 });
