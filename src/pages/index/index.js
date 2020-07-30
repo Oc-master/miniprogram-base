08a4ffe495584973c4a43853a4440120e0445c57
caixinquan <caixinquan@dfl.com.cn>
2020-07-30 08:38:51 +0800

    feat: 修改框架基础内容,保留微信与百度共通部分

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,27 +1,5 @@
 import './index.less';
 
 Page({
-  data: {
-    isShowDialog: false,
-    dialog: {
-      title: '',
-      content: '',
-    },
-  },
-  onClick(e) {
-    const dicts = {
-      host: {
-        title: '域名 hosts',
-      },
-      route: {
-        title: '路由 routes',
-      },
-    };
-    const { type } = e.target.dataset;
-    const { title } = dicts[type];
-    this.setData({
-      isShowDialog: true,
-      dialog: { title },
-    });
-  },
+  data: {},
 });
