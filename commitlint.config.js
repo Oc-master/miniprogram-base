22eda1497c6ad4c3036981c07f941699be813d89
caixinquan <caixinquan@dfl.com.cn>
2020-09-24 09:14:52 +0800

    feat: 添加commitlint配置文件

---
+++ commitlint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  /**
+   * Resolveable ids to commitlint configurations to extend
+   */
+  extends: ['@commitlint/config-conventional'],
+  /**
+   * Resolveable id to package, from node_modules, which formats the output
+   */
+  formatter: '@commitlint/format',
+  /**
+   * Rules to check against
+   */
+  rules: {
+    'header-max-length': [1, 'always', 80],
+  },
+};
