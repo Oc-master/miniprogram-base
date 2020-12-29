ddd5174da61cc22dfc9764d26a5ca3b94d2c270c
caixinquan <caixinquan@dfl.com.cn>
2020-12-29 15:43:25 +0800

    feat(utils/util.js): 完成获取页面实例功能函数

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,7 +1,7 @@
 /**
  * 防抖功能函数，常用于按钮点击类业务场景
  * @param {Function} fn 需要防抖执行的函数
- * @param {Number} delay 防抖时间间隔 默认值 300 毫秒
+ * @param {Number} delay 防抖时间间隔 默认值: 300 毫秒
  * @example
  * import { debounce } from '@/utils/util';
  * Page({
@@ -20,7 +20,7 @@ export function debounce(fn, delay = 300) {
 }
 /**
  * 获取页面实例
- * @param {Number} depth 路由栈的深度 默认值 1（默认返回当前页面实例）
+ * @param {Number} depth 路由栈的深度 默认值: 1（默认返回当前页面实例）
  */
 export function getInstance(depth = 1) {
   const pages = getCurrentPages();
