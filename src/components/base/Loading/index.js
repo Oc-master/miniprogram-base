8778a192e49e50598b4d3b469af3a315fe5ba604
caixinquan <caixinquan@dfl.com.cn>
2020-12-29 17:32:14 +0800

    feat(components/base/Loading/): 优化Loading组件功能

--- src/components/base/Loading/index.js
+++ src/components/base/Loading/index.js
@@ -4,7 +4,23 @@ Component({
   properties: {
     type: {
       type: String,
-      value: 'wave',
+      value: 'planet',
+    },
+    color: {
+      type: String,
+      value: '#c9c9c9',
+    },
+    size: {
+      type: Number,
+      value: 30,
+    },
+    textSize: {
+      type: Number,
+      value: 16,
+    },
+    vertical: {
+      type: Boolean,
+      value: false,
     },
   },
 });
